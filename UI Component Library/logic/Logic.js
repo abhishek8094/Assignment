2af1ea16d1ee9d1d12005b760af7560fd5b7a93f
abhisek8094 <iasabhi41@gmail.com>
2024-02-03 12:51:51 +0530

    Added Homepage

--- UI Component Library/logic/Logic.js
+++ UI Component Library/logic/Logic.js
@@ -1,18 +1,23 @@
-const navlist = document.querySelector(".navlist");
+const navList = document.querySelector(".navlist");
 const social = document.querySelector(".socialmedia-link");
 const hamburger = document.querySelector("#hamburger")
 const cross = document.querySelector("#cross")
 
 function showSidebar(){
-    navlist.style.display = "block";
+    navList.style.display = "block";
     social.style.display = "block";
     cross.style.display = "block";
     hamburger.style.display = "none";
 }
 
 function hideSidebar(){
-    navlist.style.display = "none";
+    navList.style.display = "none";
     social.style.display = "none";
     cross.style.display = "none";
     hamburger.style.display = "block";
+}
+
+//top function
+function topFunction() {
+    document.documentElement.scrollTop = 0;
 }
